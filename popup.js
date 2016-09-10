2a29dff21a856e17c54348349d8a5a968af39936
Joshua Miles <milesjos@msu.edu>
2016-09-10 15:38:46 -0400

    :shower: Clean up JavaScript

--- popup.js
+++ popup.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  selectedSpeed.addEventListener("click", function(event) {
-    document.getElementById("currentSpeed").innerHTML = document.getElementById("selectedSpeed").value;
+  selectedSpeed = document.getElementById("selectedSpeed");
+
+  selectedSpeed.addEventListener("change", function(event) {
+    // Get selected speed
+    selectedSpeed = this.value;
+    // Show user selected speed
+    document.getElementById("currentSpeed").innerHTML = selectedSpeed;
+
   });
 });
