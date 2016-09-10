69caf69e2506e025cf9942780be7a4a29ab97e70
Joshua Miles <milesjos@msu.edu>
2016-09-10 17:20:25 -0400

    :sparkles: Persist value on a per browser basis

--- popup.js
+++ popup.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  selectedSpeed = document.getElementById("selectedSpeed");
+  // Get selected speed element
+  var selectedSpeed = document.getElementById("selectedSpeed");
+  var speedIndicator = document.getElementById("currentSpeed");
 
+  // Add listener on change of slider
   selectedSpeed.addEventListener("change", function(event) {
     // Get selected speed
-    selectedSpeed = this.value;
-    // Show user selected speed
-    document.getElementById("currentSpeed").innerHTML = selectedSpeed;
+    var selectedSpeed = this.value;
 
+    // Save speed if different
+    chrome.storage.local.get(["speed"], function(result) {
+      if (result.speed !== selectedSpeed) {
+        chrome.storage.local.set({'speed': selectedSpeed});
+      }
+    });
+
+    // Show user selected speed
+     speedIndicator.innerHTML = selectedSpeed;
   });
+
+  // Set value to stored value
+  chrome.storage.local.get(["speed"], function(result) {
+    if (result.speed !== undefined) {
+      selectedSpeed.value = result.speed;
+      speedIndicator.innerHTML = result.speed;
+    }
+  });
+
 });
